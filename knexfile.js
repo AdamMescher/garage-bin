19d9925f742e263c2a9f7ab68162679dd2784c30
Adam Mescher <admescher@gmail.com>
2017-12-18 13:43:47 -0700

    Add Development and test enviroment

---
+++ knexfile.js
@@ -0,0 +1,24 @@
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: 'postgres://localhost/garagebin',
+    migrations: {
+      directory: './db/migrations',
+    },
+    seeds: {
+      directory: './db/seeds/dev',
+    },
+    useNullAsDefault: true,
+  },
+  test: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL || 'postgres://localhost/garagebin_test',
+    migrations: {
+      directory: './db/migrations',
+    },
+    seeds: {
+      directory: './db/seeds/test',
+    },
+    useNullAsDefault: true,
+  },
+};
