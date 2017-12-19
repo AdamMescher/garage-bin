de37967245bd2de06e3174c411cf6fde0b96c2ea
Adam Mescher <admescher@gmail.com>
2017-12-18 17:54:09 -0700

    Add dev seed file

--- db/seeds/dev/items.js
+++ db/seeds/dev/items.js
@@ -4,19 +4,19 @@ exports.seed = (knex, Promise) =>
       Promise.all([
         knex('items').insert([
           {
-            id: 1, name: 'tennis ball', reason: 'never know when a friendly dog will come and visit', cleanliness: 'dusty',
+            id: 1, name: 'tennis ball', reason: 'never know when a friendly dog will come and visit', cleanliness: 'Dusty',
           },
           {
-            id: 2, name: 'weird lamp', reason: 'could go in the basement if we ever finish it', cleanliness: 'dusty',
+            id: 2, name: 'weird lamp', reason: 'could go in the basement if we ever finish it', cleanliness: 'Dusty',
           },
           {
-            id: 3, name: 'camping gear', reason: 'not enough room in the house', cleanliness: 'sparkling',
+            id: 3, name: 'camping gear', reason: 'not enough room in the house', cleanliness: 'Sparkling',
           },
           {
-            id: 4, name: 'newspapers from 5 years ago', reason: 'never know when some newspaper could be useful', cleanliness: 'dusty',
+            id: 4, name: 'newspapers from 5 years ago', reason: 'never know when some newspaper could be useful', cleanliness: 'Dusty',
           },
           {
-            id: 5, name: 'something rancid', reason: 'no idea', cleanliness: 'rancid',
+            id: 5, name: 'something rancid', reason: 'no idea', cleanliness: 'Rancid',
           },
         ]),
       ]))
